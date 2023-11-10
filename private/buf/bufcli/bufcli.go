ad6aa4cdaefff94b88d5ccfe568a61c7f36f2ed1
oliversun9 <oliversun9@users.noreply.github.com>
2023-11-10 16:52:13 +0000

    Update version to 10.10.10

--- private/buf/bufcli/bufcli.go
+++ private/buf/bufcli/bufcli.go
@@ -65,7 +65,7 @@ import (
 
 const (
 	// Version is the CLI version of buf.
-	Version = "3.97.4-dev"
+	Version = "10.10.10"
 
 	inputHTTPSUsernameEnvKey      = "BUF_INPUT_HTTPS_USERNAME"
 	inputHTTPSPasswordEnvKey      = "BUF_INPUT_HTTPS_PASSWORD"
